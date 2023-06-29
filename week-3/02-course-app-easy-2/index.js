e461be0ca84850df204ae45b6a0577a42fc08e8e
vedansh <vedansh.dev2001@gmail.com>
2023-06-29 17:02:45 +0530

    02-course-app-easy-2 completed

--- week-3/02-course-app-easy-2/index.js
+++ week-3/02-course-app-easy-2/index.js
@@ -1,54 +1,160 @@
 const express = require('express');
+const jwt=require('jsonwebtoken');
+const secretKey="Vedansh";
 const app = express();
+
+function generateToken(username,password){
+  return jwt.sign({username:username,password:password},secretKey);
+}
+
+function verifyToken(token){
+  return jwt.verify(token,secretKey);
+}
 
 app.use(express.json());
 
 let ADMINS = [];
 let USERS = [];
 let COURSES = [];
+let Id=0;
 
 // Admin routes
 app.post('/admin/signup', (req, res) => {
   // logic to sign up admin
+  const admin=req.body;
+  const token=generateToken(admin.username,admin.password);
+  ADMINS.push({...admin,token:token});
+  res.send({message:"Admin created successfully",token:token});
 });
 
 app.post('/admin/login', (req, res) => {
-  // logic to log in admin
+  const {username,password}=req.body;
+  //check if admin exists
+  const admin = ADMINS.find((admin) => admin.username === username && admin.password === password);
+  if(admin)res.send({"message":'Logged in successfully',token:admin.token});
+  else res.send({"message":"invalid credentials"});
 });
 
 app.post('/admin/courses', (req, res) => {
-  // logic to create a course
+  try{
+    const token=req.headers.authorization.split(" ")[1];
+    const {username,password}=verifyToken(token);
+    const admin = ADMINS.find((admin) => admin.username === username && admin.password === password);
+    if(admin){
+      const course=req.body;
+      Id++;
+      COURSES.push({...course,courseID:Id});
+      res.send({"message":"Course created successfully",courseID:Id});
+    }
+    else res.send({"message":"invalid credentials"});
+    
+    
+  }catch(err){
+    res.send({"message":"invalid token"});
+  }
+
+
 });
 
 app.put('/admin/courses/:courseId', (req, res) => {
-  // logic to edit a course
+try{
+  const token=req.headers.authorization.split(" ")[1];
+  const {username,password}=verifyToken(token);
+  const courseID=req.params.courseId;
+  const admin = ADMINS.find((admin) => admin.username === username && admin.password === password);
+  if(!admin)res.send({"message":"invalid credentials"});
+  else{
+    const courseIndex=COURSES.findIndex((course)=>course.courseID==courseID);
+    COURSES[courseIndex]={...req.body,courseID:courseID};
+    res.send({"message":"Course updated successfully"});
+}
+  }
+catch(err){
+  res.send({"message":"invalid token"});
+}
+
+
 });
 
 app.get('/admin/courses', (req, res) => {
-  // logic to get all courses
+  try{
+    // console.log(req.headers);
+    const token=req.headers.authorization.split(" ")[1];
+    const {username,password}=verifyToken(token);
+    const admin = ADMINS.find((admin) => admin.username === username && admin.password === password);
+    if(!admin)res.send({"message":"invalid credentials"});
+    else res.send({courses:COURSES});
+  }catch(err){
+    res.send({"message":"invalid token"});
+  }
 });
 
 // User routes
 app.post('/users/signup', (req, res) => {
-  // logic to sign up user
+const user=req.body;
+const token=generateToken(user.username,user.password);
+USERS.push({...user,token:token});
+res.send({message:"User created successfully",token:token});
 });
 
 app.post('/users/login', (req, res) => {
-  // logic to log in user
+const {username,password}=req.body;
+const user = USERS.find((user) => user.username === username && user.password === password);
+if(user)res.send({"message":'Logged in successfully',token:user.token});
+else res.send({"message":"invalid credentials"});
 });
 
 app.get('/users/courses', (req, res) => {
-  // logic to list all courses
+  try{
+    const token=req.headers.authorization.split(" ")[1];
+    const {username,password}=verifyToken(token);
+    const user = USERS.find((user) => user.username === username && user.password === password);
+    if(!user)res.send({"message":"invalid credentials"});
+    else res.send({courses:COURSES});
+  }
+  catch(err){
+    res.send({"message":"invalid token"});
+  }
+
 });
 
 app.post('/users/courses/:courseId', (req, res) => {
-  // logic to purchase a course
+try{
+  const token=req.headers.authorization.split(" ")[1];
+  const {username,password}=verifyToken(token);
+  const courseID=req.params.courseId;
+  const user = USERS.find((user) => user.username === username && user.password === password);
+  if(!user)res.send({"message":"invalid credentials"});
+  else{
+    const course=COURSES.find((course)=>course.courseID==courseID);
+    if(user['purchasedCourses']){
+      user['purchasedCourses'].push(course);
+    }else{
+      user['purchasedCourses']=[course];
+    }
+    res.send({"message":"Course purchased successfully"});
+  }
+}
+catch(err){
+  res.send({"message":"invalid token"});
+}
+
 });
 
 app.get('/users/purchasedCourses', (req, res) => {
-  // logic to view purchased courses
+  try{
+    const token=req.headers.authorization.split(" ")[1];
+    const {username,password}=verifyToken(token);
+    const user = USERS.find((user) => user.username === username && user.password === password);
+    if(!user)res.send({"message":"invalid credentials"});
+    else res.send({coursesPurchased:user['purchasedCourses']});
+  }
+  catch(err){
+    res.send({"message":"invalid token"});
+  }
+
 });
 
 app.listen(3000, () => {
-  console.log('Server is listening on port 3000');
+console.log('Server is listening on port 3000');
 });
